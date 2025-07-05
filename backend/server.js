e4b77d16de0f28d89772d5a6b0ad0df1589227b9
Utkarsh Gupta <utkarsh.qwe@gmail.com>
2025-07-06 02:17:29 +0530

    some more changes

--- backend/server.js
+++ backend/server.js
@@ -46,8 +46,6 @@ app.delete("/api/products/:id", async (req,res) => {
     
 });
 
-// console.log(process.env.MONGO_URI);
-
 app.listen(5000, () =>{
     connectDB();
     console.log("Server started at http://localhost:5000");
