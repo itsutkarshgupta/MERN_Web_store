ceadb5bce5ba9ff845a626dc1503493e69cad303
Utkarsh Gupta <utkarsh.qwe@gmail.com>
2025-07-06 02:37:28 +0530

    CRUD APIs

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
 
@@ -44,6 +45,34 @@ app.delete("/api/products/:id", async (req,res) => {
 		res.status(500).json({ success: false, message: "Server Error" });
 	}
     
+});
+
+app.get("/api/products", async (req,res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json({success:true, data:products});
+    } catch (error) {
+        console.error("Error in fetching products:", error.message);
+        res.status(500).json({success:false, message: "internal server error"});
+    }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = req.body;
+    if (!product.name || !product.image || !product.description || !product.price) {
+        return res.status(400).json({ success: false, message: "Please fill all the fields" });
+    }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid Product Id" });
+    }
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
+        console.error("Error in updating product:", error.message);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
 });
 
 app.listen(5000, () =>{
